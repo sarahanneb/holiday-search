7fd1926e981fd8045fe6388fda3d292c8b81a115
sarahanneb <sarahb1992.1@gmail.com>
2023-08-02 08:29:25 -0700

    test: first integtation test passing!!!!!!!!

---
+++ tests/holiday.search.tests/IntegrationTests.cs
@@ -0,0 +1,39 @@
+﻿using holiday.search.Clients;
+using holiday.search.Models;
+using holiday.search.Services;
+
+namespace holiday.search.tests
+{
+    public class IntegrationTests
+    {
+        private readonly BookingService _sut;
+
+        public IntegrationTests() 
+        {
+            _sut = new BookingService(
+                new FlightDataService(new FlightDataClient()),
+                new HotelDataService(new HotelDataClient())
+                );
+        }
+
+        [Fact]
+        public void GetBookingInformation_ShouldReturn_Flight2Hotel9()
+        {
+            // Arrange
+            var request = new BookingRequestModel
+            {
+                DepartingFrom = "MAN",
+                TravellingTo = "AGP",
+                DepartureDate = "2023-07-01",
+                Duration = 7
+            };
+
+            // Act
+            var response = _sut.GetBookingInformation(request);
+
+            // Assert
+            Assert.Equal(2, response[0].FlightId);
+            Assert.Equal(9, response[0].HotelId);
+        }
+    }
+}
