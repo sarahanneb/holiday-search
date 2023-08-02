2751a368cb58895fc12b7214c7b4c0c4263c3c64
sarahanneb <sarahb1992.1@gmail.com>
2023-08-02 07:20:17 -0700

    feat: added functionality to retrieve flight and hotel data to booking service. feat: add placeholder logic to create booking response.
    test: added tests for the following scenarios:
    1. return empty list if no flights or hotels found
    2. flight and hotel data should be ordered by price ascending
    3. hotel price should be calculated by nights * price per night
    4. total price should be calculated by hotel price + flight price

--- src/holiday.search/Services/BookingService.cs
+++ src/holiday.search/Services/BookingService.cs
@@ -1,5 +1,6 @@
 ﻿using holiday.search.Models;
 using holiday.search.Services.Interfaces;
+using System.Linq.Expressions;
 
 namespace holiday.search.Services
 {
@@ -16,7 +17,62 @@ namespace holiday.search.Services
 
         public List<BookingResponseModel> GetBookingInformation(BookingRequestModel request)
         {
-            return new List<BookingResponseModel>();
+            var flights = GetFlightData(request);
+            var hotels = GetHotelData(request);
+
+            if (!flights.Any() || !hotels.Any())
+            {
+                return new List<BookingResponseModel>();
+            }
+
+            return CreateResponse(flights, hotels);
+        }
+
+        private List<FlightDataModel> GetFlightData(BookingRequestModel request)
+        {
+            Expression<Func<FlightDataModel, bool>> filter = x => 
+                x.DepartureDate == request.DepartureDate
+                && x.To == request.TravellingTo;
+
+            var filteredFlights = _flightDataService.Get(filter);
+
+            return filteredFlights == null || !filteredFlights.Any() 
+                ? new List<FlightDataModel>() 
+                : filteredFlights.OrderBy(x => x.Price).ToList();
+        }
+
+        private List<HotelDataModel> GetHotelData(BookingRequestModel request)
+        {
+            Expression<Func<HotelDataModel, bool>> filter = x => 
+                x.Nights == request.Duration
+                && x.LocalAirports.Contains(request.TravellingTo);
+
+            var filteredHotels = _hotelDataService.Get(filter);
+
+            return filteredHotels == null || !filteredHotels.Any() 
+                ? new List<HotelDataModel>() 
+                : filteredHotels.OrderBy(x => x.PricePerNight).ToList();
+        }
+
+        private static List<BookingResponseModel> CreateResponse(List<FlightDataModel> flights, List<HotelDataModel> hotels)
+        {
+            var flight = flights.First();
+            var hotel = hotels.First();
+
+            return new List<BookingResponseModel>()
+            {
+                new BookingResponseModel
+                {
+                    TotalPrice = flight.Price + (hotel.PricePerNight * hotel.Nights),
+                    FlightId = flight.Id,
+                    FlightPrice = flight.Price,
+                    FlightDepartingFrom = flight.From,
+                    FlightTravellingTo = flight.To,
+                    HotelId = hotel.Id,
+                    HotelName = hotel.Name,
+                    HotelPrice = hotel.PricePerNight * hotel.Nights
+                }
+            };
         }
     }
 }
--- tests/holiday.search.tests/Services/BookingServiceTests.cs
+++ tests/holiday.search.tests/Services/BookingServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using holiday.search.Models;
 using holiday.search.Services;
 using holiday.search.Services.Interfaces;
+using System.Linq.Expressions;
 
 namespace holiday.search.tests.Services
 {
@@ -9,10 +10,17 @@ namespace holiday.search.tests.Services
         private readonly Mock<IFlightDataService> _flightDataServiceMock = new();
         private readonly Mock<IHotelDataService> _hotelDataServiceMock = new();
 
+        private readonly List<FlightDataModel> TestFlightData = new();
+        private readonly List<HotelDataModel> TestHotelData = new();
+
         private readonly BookingService _sut;
 
         public BookingServiceTests()
         {
+            SetupTestData();
+
+
+
             _sut = new BookingService(_flightDataServiceMock.Object, _hotelDataServiceMock.Object);
         }
 
@@ -22,7 +30,122 @@ namespace holiday.search.tests.Services
             // Act
             var response = _sut.GetBookingInformation(new BookingRequestModel());
 
+            // Assert
             Assert.IsType<List<BookingResponseModel>>(response);
+        }
+
+        [Fact]
+        public void GetBookingInformation_ShouldCall_FlightAndHotelServices()
+        {
+            // Act
+            _sut.GetBookingInformation(new BookingRequestModel());
+
+            // Assert
+            _flightDataServiceMock.Verify(x => x.Get(It.IsAny<Expression<Func<FlightDataModel, bool>>>()), Times.Once);
+            _hotelDataServiceMock.Verify(x => x.Get(It.IsAny<Expression<Func<HotelDataModel, bool>>>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetBookingInformation_ShouldReturnEmptyResponseList_IfNoFlightsOrHotelsAvailable()
+        {
+            // Arrange
+            _flightDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<FlightDataModel, bool>>>())).Returns(new List<FlightDataModel>());
+            _hotelDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<HotelDataModel, bool>>>())).Returns(new List<HotelDataModel>());
+
+            // Act
+            var response = _sut.GetBookingInformation(new BookingRequestModel());
+
+            // Assert
+            Assert.Empty(response);
+        }
+
+        [Fact]
+        public void GetBookingInformation_ShouldOrderFlightsAndHotels_ByPrice()
+        {
+            // Arrange
+            _flightDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<FlightDataModel, bool>>>())).Returns(TestFlightData);
+            _hotelDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<HotelDataModel, bool>>>())).Returns(TestHotelData);
+
+            // Act
+            var response = _sut.GetBookingInformation(new BookingRequestModel());
+
+            // Assert
+            Assert.NotEmpty(response);
+            Assert.Equal(1, response.First().FlightId);
+            Assert.Equal(1, response.First().HotelId);
+        }
+
+        [Fact]
+        public void GetBookingInformation_ShouldCalculateHotelPrice_UsingPricePerNightAndNights()
+        {
+            // Arrange
+            _flightDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<FlightDataModel, bool>>>())).Returns(TestFlightData);
+            _hotelDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<HotelDataModel, bool>>>())).Returns(TestHotelData);
+
+            // Act
+            var response = _sut.GetBookingInformation(new BookingRequestModel());
+
+            // Assert
+            Assert.NotEmpty(response);
+            Assert.Equal(1000, response.First().HotelPrice);
+        }
+
+        [Fact]
+        public void GetBookingInformation_ShouldCalculateTotalPrice_UsingFlightPriceAndHotelPrice()
+        {
+            // Arrange
+            _flightDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<FlightDataModel, bool>>>())).Returns(TestFlightData);
+            _hotelDataServiceMock.Setup(x => x.Get(It.IsAny<Expression<Func<HotelDataModel, bool>>>())).Returns(TestHotelData);
+
+            // Act
+            var response = _sut.GetBookingInformation(new BookingRequestModel());
+
+            // Assert
+            Assert.NotEmpty(response);
+            Assert.Equal(1100, response.First().TotalPrice);
+        }
+
+        private void SetupTestData()
+        {
+            TestFlightData.Add(new FlightDataModel
+            {
+                Id = 1,
+                Airline = "TestAirline1",
+                From = "A",
+                To = "B",
+                Price = 100,
+                DepartureDate = "2023/02/08"
+            });
+
+            TestFlightData.Add(new FlightDataModel
+            {
+                Id = 2,
+                Airline = "TestAirline2",
+                From = "X",
+                To = "Y",
+                Price = 200,
+                DepartureDate = "2023/02/10"
+            });
+
+            TestHotelData.Add(new HotelDataModel
+            {
+                Id = 1,
+                Name = "TestHotel1",
+                LocalAirports = new List<string> { "B" },
+                PricePerNight = 100,
+                ArrivalDate = "2023/02/08",
+                Nights = 10
+            });
+
+            TestHotelData.Add(new HotelDataModel
+            {
+                Id = 2,
+                Name = "TestHotel2",
+                LocalAirports = new List<string> { "Y" },
+                PricePerNight = 200,
+                ArrivalDate = "2023/02/10",
+                Nights = 10
+            });
         }
     }
 }
